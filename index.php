fb4b02ef40706c6012449d2ebaf15f7f40ceb313
Christophe CARON <best@caron.tv>
2024-09-15 17:20:34 +0200

    minor bug
    dataTables.min.js=>datatables.min.js

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ $external = array(
     'js-bootstrap' => '<script src="assets/js/bootstrap.bundle.min.js"></script>',
     'js-dropzone' => '<script src="assets/js/dropzone.min.js"></script>',
     'js-jquery' => '<script src="assets/js/jquery-3.6.1.min.js"></script>',
-    'js-jquery-datatables' => '<script src="assets/js/dataTables.min.js"></script>',
+    'js-jquery-datatables' => '<script src="assets/js/datatables.min.js"></script>',
     'js-highlightjs' => '<script src="assets/js/highlight.min.js"></script>',
 );
 
@@ -2382,8 +2382,8 @@ $tableTheme = (FM_THEME == "dark") ? "text-white bg-dark table-dark" : "bg-white
                 <tfoot>
                     <tr>
                         <td class="gray" colspan="<?php echo (!FM_IS_WIN && !$hide_Cols) ? (FM_READONLY ? '6' :'7') : (FM_READONLY ? '4' : '5') ?>">
-						    <?php echo lng('Total space').': <span class="badge text-bg-light border-radius-0">'. fm_get_filesize(disk_total_space("$root_path")).'</span>' ?>
-						    <?php echo lng('Free space').': <span class="badge text-bg-light border-radius-0">'. fm_get_filesize(disk_free_space("$root_path")).'</span>' ?>
+							<?php echo lng('Total space').': <span class="badge text-bg-light border-radius-0">'. fm_get_filesize(disk_total_space("$root_path")).'</span>' ?>
+							<?php echo lng('Free space').': <span class="badge text-bg-light border-radius-0">'. fm_get_filesize(disk_free_space("$root_path")).'</span>' ?>
                             <?php echo lng('FullSize').': <span class="badge text-bg-light border-radius-0">'.fm_get_filesize($all_files_size).'</span>' ?>
                             <?php echo lng('File').': <span class="badge text-bg-light border-radius-0">'.$num_files.'</span>' ?>
                             <?php echo lng('Folder').': <span class="badge text-bg-light border-radius-0">'.$num_folders.'</span>' ?>
@@ -3981,7 +3981,7 @@ $isStickyNavBar = $sticky_navbar ? 'navbar-fixed' : 'navbar-normal';
         i.fa.fa-file-powerpoint-o { color:#f6712e }
         i.go-back { font-size:1.2em;color:#007bff; }
         .main-nav { padding:0.2rem 1rem;box-shadow:0 4px 5px 0 rgba(0, 0, 0, .14), 0 1px 10px 0 rgba(0, 0, 0, .12), 0 2px 4px -1px rgba(0, 0, 0, .2)  }
-        .dataTables_filter { display:none; }
+        .datatables_filter { display:none; }
         table.dataTable thead .sorting { cursor:pointer;background-repeat:no-repeat;background-position:center right;background-image:url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABMAAAATCAQAAADYWf5HAAAAkElEQVQoz7XQMQ5AQBCF4dWQSJxC5wwax1Cq1e7BAdxD5SL+Tq/QCM1oNiJidwox0355mXnG/DrEtIQ6azioNZQxI0ykPhTQIwhCR+BmBYtlK7kLJYwWCcJA9M4qdrZrd8pPjZWPtOqdRQy320YSV17OatFC4euts6z39GYMKRPCTKY9UnPQ6P+GtMRfGtPnBCiqhAeJPmkqAAAAAElFTkSuQmCC'); }
         table.dataTable thead .sorting_asc { cursor:pointer;background-repeat:no-repeat;background-position:center right;background-image:url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABMAAAATCAYAAAByUDbMAAAAZ0lEQVQ4y2NgGLKgquEuFxBPAGI2ahhWCsS/gDibUoO0gPgxEP8H4ttArEyuQYxAPBdqEAxPBImTY5gjEL9DM+wTENuQahAvEO9DMwiGdwAxOymGJQLxTyD+jgWDxCMZRsEoGAVoAADeemwtPcZI2wAAAABJRU5ErkJggg=='); }
         table.dataTable thead .sorting_desc { cursor:pointer;background-repeat:no-repeat;background-position:center right;background-image:url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABMAAAATCAYAAAByUDbMAAAAZUlEQVQ4y2NgGAWjYBSggaqGu5FA/BOIv2PBIPFEUgxjB+IdQPwfC94HxLykus4GiD+hGfQOiB3J8SojEE9EM2wuSJzcsFMG4ttQgx4DsRalkZENxL+AuJQaMcsGxBOAmGvopk8AVz1sLZgg0bsAAAAASUVORK5CYII='); }
